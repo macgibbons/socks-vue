9a765d39454d8d11dd7d8303dcdfb5eb52b6cd4d
macgibbons <macgibbons@gmail.com>
2020-04-16 21:46:35 -0500

    list rendering with v-for

--- Main.js
+++ Main.js
@@ -1,9 +1,19 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      product: 'Socks',
-      image: './assets/vmSocks-green.jpg',
-      inStock: true
-    } 
-  })
-  
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green.jpg',
+    inventory: 2,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'    
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ]
+  } 
+})
